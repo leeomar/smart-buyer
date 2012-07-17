683da08ba9d6610dd515c1be8b0e6761c3e5c4ab
scrapyer <scrapyer@dfs00211.soniu.com>
2012-07-17 10:52:19 +0800

    增加脚本的高亮显示

--- spider/scrapyd/startScrapyd.sh
+++ spider/scrapyd/startScrapyd.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-#export PYTHONPATH=$PYTHONPATH
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
+#30:黑 31:红 32:绿 33:黄 34:蓝色 35:紫色 36:深绿 37:白色 
+warn(){
+    echo -e "\033[0;31;1m $(date "+%Y-%m-%d %H:%M:%S") [warn]: $1 \033[0m"
+}
 
-SCRAPY_HOME="/home/newbie/workspace/scrapy/"
+info(){
+    echo -e "\033[0;32;1m $(date "+%Y-%m-%d %H:%M:%S") [info]: $1 \033[0m"
+}
+
+debug(){
+    echo -e "\033[0;30;1m $(date "+%Y-%m-%d %H:%M:%S") [debug]: $1 \033[0m"
+}
+
+#SCRAPY_HOME="/home/newbie/workspace/scrapy/"
+SCRAPY_HOME="/home/scrapyer/scrapy-0-12/"
 nohup twistd -ny $SCRAPY_HOME/extras/scrapyd.tac 1>logs/scrapyd.log 2>logs/scrapyd.err &
 sleep 1
-if [ -f twistd.pid ]; then
-    echo "start scrapyd[`cat twistd.pid`]"
+if [ -f "twistd.pid" ]; then
+    info "start scrapyd[`cat twistd.pid`]"
 else
-    echo "start scrapyd"
+    warn "fail start scrapyd"
 fi
--- spider/scrapyd/stopScrapyd.sh
+++ spider/scrapyd/stopScrapyd.sh
@@ -1,11 +1,52 @@
-#/bin/sh
+#/bin/bash
 
-if [ -f twistd.pid ]; then
-    pid=`cat twistd.pid`
-    echo "stop scrapyd[$pid]"
-    kill $pid
+#30:黑 31:红 32:绿 33:黄 34:蓝色 35:紫色 36:深绿 37:白色 
+warn(){
+    echo -e "\033[0;31;1m $(date "+%Y-%m-%d %H:%M:%S") [warn]: $1 \033[0m"
+}
+
+info(){
+    echo -e "\033[0;32;1m $(date "+%Y-%m-%d %H:%M:%S") [info]: $1 \033[0m"
+}
+
+debug(){
+    echo -e "\033[0;30;1m $(date "+%Y-%m-%d %H:%M:%S") [debug]: $1 \033[0m"
+}
+
+kill_thread(){
+    pid=$1
+    force_stop=$2
+
+    ps -efww | grep $pid | grep -v grep
+    if [ $? -ne 0 ]; then
+        warn "no such thread[$pid]"
+        exit 1
+    fi
+
+    if $force_stop; then
+        info 'force stop thread'
+        kill -9 $pid
+    else
+        kill $pid
+    fi
+
     sleep 2
-    ps ux | grep $pid | grep -v grep
+    ps -efww | grep $pid | grep -v grep
+    if [ $? -eq 0 ]; then
+        warn "fail stop thread[$pid]"
+    else
+        info "succ stop thread[$pid]"
+    fi
+}
+
+force_stop=false
+if [ $1 ];then
+    force_stop=true
+fi
+
+if [ -f 'twistd.pid' ]; then
+    pid=`cat twistd.pid`
+    kill_thread $pid $force_stop
 else
-    echo 'no twistd.pid'
+    warn 'twistd.pid not found'
 fi
