5faea79a19806ca74f861cefdcb278c169e12c6e
Falood <falood@gmail.com>
2012-08-07 23:49:19 +0800

    更改 manifest 文件为 version 2 风格

--- plugins/chrome/static/js/background.js
+++ plugins/chrome/static/js/background.js
@@ -2,7 +2,7 @@ $(function(){
     chrome.extension.onRequest.addListener(
         function(request, sender, callback){
             if ('getprice' == request.type){
-                var req = $.getJSON("http://chrome.tuanzz.com/price_trend", {
+                var req = $.getJSON('http://chrome.tuanzz.com/price_trend', {
                     url: request.data
                 }, callback);
                 req.error(callback);
